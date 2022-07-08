557ae927761c64483daf5231b556dc0003d72429
diegoturchi95 <98837781+diegoturchi95@users.noreply.github.com>
2022-07-08 15:28:39 +0200

    trying with different aar

--- app/src/main/java/com/example/okhttptest/MainActivity.kt
+++ app/src/main/java/com/example/okhttptest/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.Toast
+import com.github.arturogutierrez.Badges
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -12,14 +13,15 @@ import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.Response
 
-import com.overlook.android.fingkit.FingScanner
+//import com.overlook.android.fingkit.FingScanner
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Toast.makeText(this@MainActivity, "New func added sss", Toast.LENGTH_LONG).show()
-        val scanner: FingScanner = FingScanner.getInstance()
+        //val scanner: FingScanner = FingScanner.getInstance()
+        Badges.setBadge(this, 5)
         findViewById<Button>(R.id.btn_api_resp).setOnClickListener {
             Toast.makeText(this@MainActivity, "New func added eee", Toast.LENGTH_LONG).show()
             CoroutineScope(Dispatchers.IO).launch {
